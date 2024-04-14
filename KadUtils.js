ad53fc4aa10c4b7f5febf6ec67b5cc9b966d465c
klausHe <klausheuken@gmx.de>
2024-04-14 13:53:59 +0200

    deleted socket

--- KadUtils.js
+++ KadUtils.js
@@ -1,22 +1,3 @@
-export async function socketPost(app = null, data, callback) {
-	if (app == null) return;
-	if (typeof data != "object") {
-		error("NO OBJECT'", data);
-	}
-	const path = "K-Universe/";
-	const request = new Request(`${path}${app}/`, {
-		method: "POST",
-		body: data == null ? "" : JSON.stringify(data),
-		headers: {
-			Accept: "application/json",
-			"Content-Type": "application/json",
-		},
-	});
-	const response = await fetch(request);
-	const obj = await response.json();
-	callback(obj);
-}
-
 export function dbID(id) {
 	if (id instanceof Object) return id;
 	return document.getElementById(id);
